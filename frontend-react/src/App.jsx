f409004d46a1833bc470d9ecb760669475b16e15
shashanksola <solashashank1010@gmail.com>
2024-07-20 15:23:28 +0530

    added dropzone functionality to upload files on the frontend

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,12 +1,12 @@
 import { Editor } from '@monaco-editor/react'
-import FileStruct from './conmponents/FileStruct/FileStruct'
+import FileStruct from './conmponents/FileStruct'
 import './App.css'
 
 function App() {
   return (
     <div className='main-container'>
       <FileStruct />
-      <Editor height='70vh' width='100%' theme='vs-dark' language='python' />
+      <Editor height='100vh' width='70vw' theme='vs-dark' language='python' />
     </div>
   )
 }
---
+++ frontend-react/src/conmponents/FileStruct.jsx
@@ -0,0 +1,14 @@
+import DropBox from './DropBox';
+
+function FileStruct() {
+    return (
+        <div className='non-editor-content'>
+            <div className="file-tree">
+                HI
+            </div>
+            <DropBox />
+        </div>
+    )
+}
+
+export default FileStruct;
